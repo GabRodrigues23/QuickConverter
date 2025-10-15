60462457d1cdf6495759852b1a0c64aee636bc78
GabRodrigues23 <gabrigues23@gmail.com>
2025-10-15 14:59:14 -0300

    :sparkles: created frontend files structure with flutter

---
+++ frontend/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quick Converter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      // No futuro, aqui entrará ConverterPage().
+      home: const Scaffold(
+        body: Center(
+          child: Text('Quick Converter App'),
+        ),
+      ),
+    );
+  }
+}
