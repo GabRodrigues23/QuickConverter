0b85c730ee0ead8284226b2962f9e0dc7ebd1c83
GabRodrigues23 <gabrigues23@gmail.com>
2025-10-15 22:35:13 -0300

    :sparkles: feat: create ConverterViewModel, constructors, try-catches and methods to communicate with repository and possibilite the conversion in converter_viewmodel.dart

---
+++ frontend/lib/ui/viewmodel/converter_viewmodel.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/data/model/conversion_result.dart';
+import 'package:frontend/data/repository/conversion_repository.dart';
+
+class ConverterViewModel extends ChangeNotifier {
+  final ConversionRepository _repository;
+
+  ConversionResult? _conversionResult;
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  // Getters para acessar os estados
+  ConversionResult? get conversionResult => _conversionResult;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  // Construtor com Injeção de Dependência (Repository)
+  ConverterViewModel(this._repository);
+
+  Future<void> performConversion(
+      {required String from,
+      required String to,
+      required String amount}) async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+    try {
+      // Chama o repositório para realizar a "conversão"
+      final result = await _repository.convert(
+        from: from,
+        to: to,
+        amount: amount,
+      );
+
+      _conversionResult = result;
+    } catch (e) {
+      // Caso ocorra algum erro (simulado ou real futuramente)
+      _errorMessage = 'Erro ao realizar conversão: $e';
+    } finally {
+      // Atualiza o estado final (sempre executado)
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
