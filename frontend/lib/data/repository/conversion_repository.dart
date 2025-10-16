120ef52e82d77e065f0b5e61a7b9d7e896311749
GabRodrigues23 <gabrigues23@gmail.com>
2025-10-16 00:18:40 -0300

    :recycle: refactor parameters, change to required, add a print do debug in repository

--- frontend/lib/data/repository/conversion_repository.dart
+++ frontend/lib/data/repository/conversion_repository.dart
@@ -5,13 +5,16 @@ class ConversionRepository {
   // Método responsável por realizar a "conversão"
   Future<ConversionResult> convert({
     required String from,
-    to,
-    amount,
+    required String to,
+    required String amount,
   }) async {
+    // Print para facilitar o debug (exibe os parâmetros recebidos)
+    print('Iniciando conversão: $amount de $from para $to');
+
     // Simula o tempo de resposta da rede.
     await Future.delayed(const Duration(seconds: 2));
 
-    // Retorna um resultado fictício
+    // Retorna um resultado fictício (mocked)
     return ConversionResult(
       originalAmount: amount,
       fromCurrency: from,
