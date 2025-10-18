027e8fdb3d8f4121bba799110d805ecb011bee88
GabRodrigues23 <gabrigues23@gmail.com>
2025-10-17 21:07:15 -0300

    :sparkles: add app.dart to manage app ui and change main.dart junst to call app.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,30 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'data/repository/conversion_repository.dart';
-import 'ui/view/converter_page.dart';
-import 'ui/viewmodel/converter_viewmodel.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ConverterViewModel(ConversionRepository()),
-      child: MaterialApp(
-        title: 'Quick Converter',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-          useMaterial3: true,
-        ),
-        home: const ConverterPage(),
-      ),
-    );
-  }
+  runApp(const QuickConverterApp());
 }
